144c7c72d40e8d02e0af8f5cd8f8f9dce1ae0d29
Chris <c.underwood@digipen.edu>
2019-01-26 21:01:31 -0800

    Added T H E B O I

---
+++ Bigboi Monopoly/Assets/Scripts/RandomEventButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomEventButton : MonoBehaviour
+{
+    public string[] RandomEvents;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void TextChanger()
+    {
+        int index = Random.Range(0, RandomEvents.Length);
+        GetComponent<Text>().text = RandomEvents[index];
+    }
+}
