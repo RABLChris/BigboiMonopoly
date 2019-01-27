996dfa4b184f26ca27bc615fa1c78435f333e3cf
picoriley <acloudy@mail.smu.edu>
2019-01-26 22:59:40 -0800

    The robot is angerdy

---
+++ Bigboi Monopoly/Assets/Scripts/ShaderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShaderController : MonoBehaviour
+{
+    bool robotModeActivated = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleRobotMode()
+    {
+        robotModeActivated = !robotModeActivated;
+        GetComponent<Image>().material.SetFloat("_RobotMode", robotModeActivated ? 1.0f : 0.0f);
+    }
+}
