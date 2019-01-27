e2013769d3cd983ed0eecd34cdf1fab62a36f3b8
picoriley <acloudy@mail.smu.edu>
2019-01-27 00:45:12 -0800

    Moneybags Is Back Baby

---
+++ Bigboi Monopoly/Assets/Scripts/MrMoneybags.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MrMoneybags : MonoBehaviour
+{
+    private float timeSinceRotation = 1.0f;
+    private Quaternion targetRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceRotation += Time.deltaTime;
+        if (timeSinceRotation >= 1.0f)
+        {
+            GetComponent<Transform>().rotation = targetRotation;
+            targetRotation = Quaternion.Euler(Random.onUnitSphere * 180.0f);
+            timeSinceRotation = 0.0f;
+        }
+        GetComponent<Transform>().rotation = Quaternion.Slerp(GetComponent<Transform>().rotation, targetRotation, timeSinceRotation);
+    }
+}
