efff52cd4215bb3fff1232c2687b1925bf7fc53c
picoriley <acloudy@mail.smu.edu>
2019-01-27 01:37:25 -0800

    We did it Reddit.

--- Bigboi Monopoly/Assets/Scripts/MrMoneybags.cs
+++ Bigboi Monopoly/Assets/Scripts/MrMoneybags.cs
@@ -32,7 +32,7 @@ public class MrMoneybags : MonoBehaviour
     public void OOH()
     {
         var voice = GetComponent<AudioSource>();
-        voice.pitch = Random.Range(0.8f, 1.2f);
+        voice.pitch = ShaderController.robotModeActivated ? Random.Range(1.2f, 1.8f) : Random.Range(0.8f, 1.2f);
         voice.Play(0);
     }
 }
